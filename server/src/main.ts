1f96abaafc1b325d708f3267444c8f7dbe9ff136
nkfrv1 <i.nikiforov1111@gmail.com>
2023-05-07 13:32:43 +0300

    cors configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: [process.env.CLIENT_URL],
+        credentials: true,
+    });
     await app.listen(process.env.PORT || 3000);
 }
 

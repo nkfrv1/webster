4e141282119c6736787552a7cae9041e616743c9
nkfrv1 <i.nikiforov1111@gmail.com>
2023-05-10 19:06:36 +0300

    connected db & orm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [ConfigModule.forRoot({ expandVariables: true })],
+    imports: [
+        ConfigModule.forRoot({ expandVariables: true }),
+        MongooseModule.forRoot(process.env.MONGODB_CONNECTION_URI),
+    ],
 })
 export class AppModule {}

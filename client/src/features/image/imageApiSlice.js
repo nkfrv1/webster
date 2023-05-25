5f588d93aac2ca0583706a3a060e704229ff9a77
Yura Myros <yura.myros1604@gmail.com>
2023-05-25 17:56:39 +0300

    users and images api slices

---
+++ client/src/features/image/imageApiSlice.js
@@ -0,0 +1,70 @@
+import { apiSlice } from '../../app/api/apiSlice';
+
+export const imageApiSlice = apiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		saveImage: builder.mutation({
+			query: (credentials) => {
+				const formData = new FormData();
+				formData.append('file', credentials.file, credentials.file.name);
+
+				const config = {
+					headers: {
+						'Content-Type': 'multipart/form-data',
+					},
+				};
+
+				return {
+					url: `/images`,
+					method: 'POST',
+					body: formData,
+					config,
+				};
+			},
+		}),
+		getImages: builder.query({
+			query: () => ({
+				url: '/images',
+				method: 'GET',
+			}),
+		}),
+		getImage: builder.query({
+			query: (id) => ({
+				url: `/images/${id}`,
+				method: 'GET',
+			}),
+		}),
+		patchImage: builder.mutation({
+			query: (credentials) => {
+				const formData = new FormData();
+				formData.append('file', credentials.file, credentials.file.name);
+
+				const config = {
+					headers: {
+						'Content-Type': 'multipart/form-data',
+					},
+				};
+
+				return {
+					url: `/images/${credentials.id}`,
+					method: 'POST',
+					body: formData,
+					config,
+				};
+			},
+		}),
+		deleteImage: builder.mutation({
+			query: (id) => ({
+				url: `/images/${id}`,
+				method: 'DELETE',
+			}),
+		}),
+	}),
+});
+
+export const {
+	useSaveImageMutation,
+	useGetImagesQuery,
+	useGetImageQuery,
+	usePatchImageMutation,
+	useDeleteImageMutation,
+} = imageApiSlice;
